982e48eea68e1577b31918e21b430a17cae7fc8d
John Chia <john.chia0106@gmail.com>
2022-02-01 19:13:12 -0800

    added the interactive counter that counts the amount of letters of the input

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -11,11 +11,13 @@ $(() => {
   const $mainlist = $('#main-list');
   const $inputButton = $('#tweet-button');
 
-  $inputButton.on('click', () => {
+  $inputButton.on('click', (event) => {
+    event.preventDefault();
     const $inputField = $('#tweet-text');
     const $inputValue = $inputField.val();
     const $div = $('<div class="new-tweet">').text($inputValue);
     $mainlist.append($div);
     $inputField.val('').focus();
+    $('.counter').html(140); 
   });
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,21 @@
 
 
 
-const keypress = document.getElementById('tweet-text');
+const input = document.getElementById('tweet-text');
 
 
-$(keypress).on('keypress', function() {
-  // $("output").text("--");
-  console.log(this);
+$(input).on('input', function() {
+  let count = Number($(this).val().length)
+
+  if (count > 140) {
+    $('.counter').css('color', '#8B0000');
+    $('.counter').html(140 - count);
+  } else {
+    $('.counter').html(140 - count);
+  }
 });
 
+
+$(input).on('input', function() {
+  console.log($(".counter"));
+});
